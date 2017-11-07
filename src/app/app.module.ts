63da40b143ac6bf34554cd513e8b38f4d91b43d6
Anshul Sanghi <anshap1719@gmail.com>
2017-11-07 21:36:24 +0530

    Finalize Project Setup, Add Basic Components, Add Icon Files, Setup Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './global/header/header.component';
 import { FooterComponent } from './global/footer/footer.component';
 import { ImportListingComponent } from './base/import-listing/import-listing.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AngularMaterialModule} from "./angular-material/angular-material.module";
+import {RouterModule} from "@angular/router";
+import { LoginComponent } from './base/login/login.component';
+import { SignupComponent } from './base/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ImportListingComponent
+    ImportListingComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+      { path: '', component: ImportListingComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/base/import-listing/import-listing.component.ts
+++ src/app/base/import-listing/import-listing.component.ts
@@ -12,4 +12,8 @@ export class ImportListingComponent implements OnInit {
   ngOnInit() {
   }
 
+  importListing() {
+
+  }
+
 }
